914110680c73c79164a227500469f4a097ff2135
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-03-13 10:29:45 +0100

    Fix issue #6

--- victimsdb_lib/__init__.py
+++ victimsdb_lib/__init__.py
@@ -2,3 +2,8 @@
 
 from victimsdb_lib.database import VictimsDB
 from victimsdb_lib.errors import VictimsDBError, ParseError
+
+# to fix dead code detector issues
+assert VictimsDB
+assert VictimsDBError
+assert ParseError
