0c39a8d0207a21d3ad950e4a3db491ab3c93729c
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-04-09 08:08:30 +0200

    Updated script to run tests

--- runtests.sh
+++ runtests.sh
@@ -4,28 +4,34 @@ set -e
 
 COVERAGE_THRESHOLD=90
 
-
 echo "Create Virtualenv for Python deps ..."
+
 function prepare_venv() {
-    VIRTUALENV="$(which virtualenv)"
-    if [ $? -eq 1 ]
-    then
-        # python34 which is in CentOS does not have virtualenv binary
-        VIRTUALENV="$(which virtualenv-3)"
-    fi
-    if [ $? -eq 1 ]; then
-        # still don't have virtual environment -> use python3.4 directly
-        python3.4 -m venv venv && source venv/bin/activate && python3 "$(which pip3)" install -r integration_tests/requirements.txt && python3 "$(which pip3)" install -r tests/requirements.txt && python3 "$(which pip3)" install -r requirements.txt && python3 "$(which pip3)" install git+https://github.com/fabric8-analytics/fabric8-analytics-worker.git@561636c
-    else
-        ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    #VIRTUALENV="$(which virtualenv)"
+    python3.4 -m venv venv && source venv/bin/activate
+    # TODO: fix/delete after update to Python 3.6!!!
+    #if [ $? -eq 1 ]
+    #then
+    #    # python34 which is in CentOS does not have virtualenv binary
+    #    echo "Trying to find virtualenv-3"
+    #    VIRTUALENV="$(which virtualenv-3)"
+    #fi
+    #if [ $? -eq 1 ]
+    #then
+    #    # still don't have virtual environment -> use python3.4 directly
+    #    echo "Fallback to Python 3.4"
+    #    python3.4 -m venv venv && source venv/bin/activate
+    #else
+    #    echo "Using ${VIRTUALENV}"
+    #    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    #fi
     if [ $? -ne 0 ]
     then
-        printf "%sPython virtual environment can't be initialized%s" "${RED}" "${NORMAL}"
+        printf "%sPython virtual environment can't be initialized%s\n" "${RED}" "${NORMAL}"
         exit 1
     fi
     pip install -U pip
     python3 "$(which pip3)" install -r requirements.txt
-
 }
 
 [ "$NOVENV" == "1" ] || prepare_venv || exit 1
