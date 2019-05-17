a70ea9df512f19b106faad824226c1138031fc6e
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-05-17 10:06:38 +0200

    Use Python 3.6 on CI

--- runtests.sh
+++ runtests.sh
@@ -7,24 +7,22 @@ COVERAGE_THRESHOLD=90
 echo "Create Virtualenv for Python deps ..."
 
 function prepare_venv() {
-    #VIRTUALENV="$(which virtualenv)"
-    python3.4 -m venv venv && source venv/bin/activate
-    # TODO: fix/delete after update to Python 3.6!!!
-    #if [ $? -eq 1 ]
-    #then
-    #    # python34 which is in CentOS does not have virtualenv binary
-    #    echo "Trying to find virtualenv-3"
-    #    VIRTUALENV="$(which virtualenv-3)"
-    #fi
-    #if [ $? -eq 1 ]
-    #then
-    #    # still don't have virtual environment -> use python3.4 directly
-    #    echo "Fallback to Python 3.4"
-    #    python3.4 -m venv venv && source venv/bin/activate
-    #else
-    #    echo "Using ${VIRTUALENV}"
-    #    ${VIRTUALENV} -p python3 venv && source venv/bin/activate
-    #fi
+    VIRTUALENV="$(which virtualenv)"
+    if [ $? -eq 1 ]
+    then
+        # python34 which is in CentOS does not have virtualenv binary
+        echo "Trying to find virtualenv-3"
+        VIRTUALENV="$(which virtualenv-3)"
+    fi
+    if [ $? -eq 1 ]
+    then
+        # still don't have virtual environment -> use python3.4 directly
+        echo "Fallback to Python 3.4"
+        python3.4 -m venv venv && source venv/bin/activate
+    else
+        echo "Using ${VIRTUALENV}"
+        ${VIRTUALENV} -p python3 venv && source venv/bin/activate
+    fi
     if [ $? -ne 0 ]
     then
         printf "%sPython virtual environment can't be initialized%s\n" "${RED}" "${NORMAL}"
